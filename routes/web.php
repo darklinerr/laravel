95612f8166a9a3303c93635e838beb46255a3d84
Denis Listopadov <listopadov.den@gmail.com>
2017-10-12 19:55:14 +0300

    admin

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>'auth'], function(){
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
