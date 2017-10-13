9dc1af6bccfe2f5b9f8efe0c5a20c2747b6b3509
Denis Listopadov <listopadov.den@gmail.com>
2017-10-13 15:22:54 +0300

    Create category ready

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -26,6 +26,11 @@ class CategoryController extends Controller
     public function create()
     {
         //
+        return view('admin.categories.create', [
+            'category' => [],
+            'categories' => Category::with('children')->where('parent_id', '0')->get(),
+            'delimiter' => ''
+        ]);
     }
 
     /**
@@ -36,7 +41,9 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Category::create($request->all());
+
+        return redirect()->route('admin.category.index');
     }
 
     /**
@@ -58,7 +65,11 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.categories.edit', [
+            'category' => $category,
+            'categories' => Category::with('children')->where('parent_id', '0')->get(),
+            'delimiter' => ''
+        ]);
     }
 
     /**
